903ae8a270c245a425f169fc1336308eefc601bf
Caroline B Flanagan <carolineflan@gmail.com>
2020-06-22 18:55:59 -0400

    sequelizestretch

--- recipe.js
+++ recipe.js
@@ -1,7 +1,20 @@
 const db = require('./dbConfig');
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 //YOUR CODE GOES HERE
+const Recipe = db.define('Recipe', {
+    name : {
+        type: DataTypes.STRING,
+        allowEmpty: false,
+    },
+    cookTime: {
+       type: DataTypes.INTEGER,
 
+    },
+
+    vegan: {
+        type: DataTypes.BOOLEAN,
+    }
+})
 //--------------------
 module.exports = Recipe;
